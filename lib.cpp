8c55b6b01ffcf69d9e121fb73dd5b42ba3cdc513
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-08 23:25:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool convL (char & a) {
+    if ((a>=65 && a <= 90) || (a>=97 && a<=122)) {
+        if (a>=65 && a<=90) {
+            a+=32;
+        } else {
+            a-=32; 
+        } return 1;
+    } else {
+        return 0;
+    }
+}
