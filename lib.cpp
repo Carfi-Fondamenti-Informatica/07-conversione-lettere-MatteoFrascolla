91684f7ee23516851341d0a609f0526822980c66
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-08 23:20:15 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,1 @@
 
-#include "lib.h"
-bool checkinput (int x){
-  if((x>=65 && x<=90) || (x>=97 && x<=122))
-    return true;
-  else
-    return false;
-}
-char conversion (char x){
-  char res;
-  if (x >='A' && x <= 'Z')
-    res = x + 32;
-  else if (x >= 'a' && <= 'z')
-    res = x - 32;
-  return res;
-}
