b35dee21495580f19a626063070bf5d04261cb41
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-08 23:20:51 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+int main() {
+    char a;
+    cin >>a;
+    if (convL(a)){
+        cout<<a<<endl;
+    }else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
+}
