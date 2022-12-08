8be8d3fada257e1a26e69c0457a5fe4faf1c39ad
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-08 22:51:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+#include "lib.h"
+bool convert (char & a){
+  if (a>065 && a<=90){
+    a +=32;
+    return true;
+  } else if (a>=97 && a<=122){
+    a -=32;
+    return true;
+  }
+  return false;
+}
