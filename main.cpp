0114819357165b58f0f68dba2273b194df5b0c65
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-08 23:19:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include "lib.h"
-using namespace std;
 
-int main(){
-  char a=0;
-  cin>>a;
-  if (convert(a)){
-    cout<<a<<endl;
-  }else{
-    cout<<"errore"<<endl;
-  }
-  return 0;
-}
